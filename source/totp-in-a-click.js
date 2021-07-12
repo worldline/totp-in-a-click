845512275ba64295985a2dff63ae58295acbad1c
Romain Philibert <romain.philibert@worldline.com>
2021-07-12 11:48:18 +0000

    Parse otpauth URL

--- source/totp-in-a-click.js
+++ source/totp-in-a-click.js
@@ -1,6 +1,7 @@
 /* global __INJECTIBLE_CODE__: readonly */
 
 import totp from 'totp-generator';
+import parseURI from 'otpauth-uri-parser';
 
 // Action listener to redirect user to source repo
 browser.browserAction.onClicked.addListener(() => {
@@ -19,14 +20,28 @@ browser.contextMenus.create({
 // Listener for events from context menus
 browser.contextMenus.onClicked.addListener(async (info, tab) => {
 	let htmlContent = '';
+	let code = ''
 
 	const completionData = await browser.tabs.executeScript(tab.id, {
 		frameId: info.frameId,
 		code: __INJECTIBLE_CODE__ // Replaced by webpack with actual code
 	});
 
-	htmlContent = completionData[0] || '';
-	const code = totp(htmlContent.trim());
+	htmlContent = (completionData[0] || '').trim();
+	if (htmlContent.startsWith('otpauth://')) {
+		const parsed = parseURI(htmlContent)
+		const secret = parsed["query"]["secret"]
+		const digits = Number(parsed["query"]["digits"]) || 6
+		const algorithm = parsed["query"]["algorithm"] || "SHA-1"
+		const period = Number(parsed["query"]["period"]) || 30
+		code = totp(secret, {
+			digits: digits,
+			algorithm: algorithm,
+			period: period,
+		})
+	}else{
+		code = totp(htmlContent);
+	}
 
 	const inputElement = document.createElement('textarea');
 	document.body.append(inputElement);
